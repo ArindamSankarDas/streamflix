ea76d58fa97dc3b934424aa533a252c0a2b473e8
Arindam Sankar Das <arindamsankar@outlook.com>
2023-05-06 15:23:18 +0530

    removed query client

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,9 @@ import "./index.css";
 
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
   </React.StrictMode>
 );
