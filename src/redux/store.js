a9e5a6f1072157bac1c5b38780b51a544c75f131
Arindam Sankar Das <arindamsankar@outlook.com>
2025-01-25 17:07:53 +0530

    removed userReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "./modalReducer/modal.reducer";
-import userReducer from "./userReducer/user.reducer";
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
-    user: userReducer,
   },
 });
